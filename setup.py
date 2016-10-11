c61db76e248a3e685206a4f43739ea6d94e8d902
Scott Wilson <scott.wilson@gmail.com>
2016-10-11 00:15:25 -0700

    Integrating py.test in setup.py.

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
+
+requirements = open('requirements.txt').readlines()
 
 setup(name='Spoonerist',
       version='0.1',
       description='Spoonerism Tool',
       author='Scott Wilson',
-      author_email='scott.wilson@gmai;.com',
+      author_email='scott.wilson@gmail.com',
+      test_suite='tests',
+      setup_requires=['pytest-runner', *requirements],
+      tests_require=['pytest'],
       url='https://github.com/scottynomad/spoonerist/',
-      packages=['distutils', 'distutils.command'],
+      packages=['spoonerist'],
      )
