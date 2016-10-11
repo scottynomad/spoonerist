7c4413fb278c9184366e4ed96a183c7dd3787645
Scott Wilson <scott.wilson@gmail.com>
2016-10-11 00:04:45 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Spoonerist',
+      version='0.1',
+      description='Spoonerism Tool',
+      author='Scott Wilson',
+      author_email='scott.wilson@gmai;.com',
+      url='https://github.com/scottynomad/spoonerist/',
+      packages=['distutils', 'distutils.command'],
+     )
