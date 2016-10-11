0690e42ebee3b8c3a46c8133031d682979fd0d28
Scott Wilson <scott.wilson@gmail.com>
2016-10-10 23:06:41 -0700

    Updated lets encrypt verification.

--- spoonerist/app.py
+++ spoonerist/app.py
@@ -19,11 +19,12 @@ class SpoonerismPairs(Resource):
 api.add_resource(SpoonerismPairs, '/<string:word>/pairs')
 
 
-@app.route('/.well-known/acme-challenge/L0JbnCYn4E8uGTxjnfn9keJ7hfwTNH19qmyCJnbopAs',
+before continuing:
+
+@app.route('/.well-known/acme-challenge/dSDMF4ky0nzegTbVXAmhkfgxCoK5YqxG4jsgq-LQSJI',
            methods=['GET'])
 def acme_challenge():
-    return ('L0JbnCYn4E8uGTxjnfn9keJ7hfwTNH19qmyCJnbopAs.0ana0d6lzmt'
-            '-Y-jEARBOXKrIX--I_9V_E7m-pbYIuXA')
+    return 'dSDMF4ky0nzegTbVXAmhkfgxCoK5YqxG4jsgq-LQSJI.0ana0d6lzmt-Y-jEARBOXKrIX--I_9V_E7m-pbYIuXA'
 
 
 if __name__ == '__main__':
